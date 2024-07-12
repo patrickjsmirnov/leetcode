00b6a94c8798e1f8c644ae0d157d9cd81806e238
Dmitry Smirnov <patrickjsmirnov@gmail.com>
2024-07-12 13:58:36 +0200

    some linked lists problems

--- src/linked-lists.js
+++ src/linked-lists.js
@@ -75,6 +75,101 @@ class LinkedList {
     }
   }
 
+  deleteDuplicates() {
+    if (!this.head) return this.head;
+
+    let current = this.head;
+
+    while (current && current.next) {
+      if (current.data === current.next.data) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+  }
+
+  isPalindrome() {
+    const data = [];
+    let current = this.head;
+    let result = true;
+
+    while (current) {
+      data.push(current.data);
+      current = current.next;
+    }
+
+    let left = 0;
+    let right = data.length - 1;
+
+    while (left < right && result) {
+      if (data[left] !== data[right]) {
+        result = false;
+      } else {
+        left++;
+        right--;
+      }
+    }
+
+    console.log(result);
+  }
+
+  isPalindromeChatGPT() {
+    if (!this.head || !this.head.next) return true;
+
+    // Step 1: Find the middle of the linked list
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // Step 2: Reverse the second half of the linked list
+    let prev = null;
+    let curr = slow;
+    while (curr) {
+      let nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
+    }
+
+    // Step 3: Compare the first and second halves
+    let firstHalf = head;
+    let secondHalf = prev; // `prev` is now the head of the reversed second half
+    while (secondHalf) {
+      if (firstHalf.val !== secondHalf.val) {
+        return false;
+      }
+      firstHalf = firstHalf.next;
+      secondHalf = secondHalf.next;
+    }
+
+    return true;
+  }
+
+  middleNode() {
+    let current = this.head;
+    let total = 1;
+    let count = 1;
+
+    while (current && current.next) {
+      current = current.next;
+      total++;
+    }
+
+    const mid = Math.floor(total / 2) + 1;
+
+    current = this.head;
+    while (count < mid) {
+      current = current.next;
+      count++;
+    }
+
+    this.head = current;
+  }
+
   reverse() {
     let prev = null;
     let current = this.head;
@@ -112,7 +207,12 @@ const linkedList = new LinkedList();
 // console.log(linkedList.find(2));
 // linkedList.delete(1);
 
-linkedList.arrayToLinkedList([1, 2, 3, 4, 5]);
+// linkedList.arrayToLinkedList([1, 1, 2, 2, 3]);
+// linkedList.arrayToLinkedList([1, 2, 2]);
+// linkedList.arrayToLinkedList([1, 1]);
+// linkedList.arrayToLinkedList([1, 2, 2, 1]); // true
+linkedList.arrayToLinkedList([1, 2]); // false
+// linkedList.arrayToLinkedList([1, 1, 2, 3, 3]);
 linkedList.print();
-linkedList.reverse();
-linkedList.print();
+linkedList.isPalindrome();
+// linkedList.print();
